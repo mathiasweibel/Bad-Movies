c1b7cf4d63f5fcb8d9cb40c150f8e66dbd18d855
Mathias <mathias2702@gmail.com>
2020-04-24 11:36:14 -0400

    Save progress on API helpers

--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -15,7 +15,7 @@ const { API_KEY } = require('../../config.js')
 module.exports = {
 
   // Get search data
-  // Add after API_KEY: &language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false&page=1
+  // Add after API_KEY: 
   getApiGenres = (next) => {
     axios
       .get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`)
@@ -32,15 +32,8 @@ module.exports = {
   }
 
   // TODO: Get data by genre
-  getApiDataByGenre = (genre) => {
-    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`, {
-      params: {
-        sort_by: vote_average.asc,
-        include_adult: false,
-        include_video: false,
-        page: 1
-      }
-    })
+  getApiDataByGenre = (genreId) => {
+    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false&page=1&with_genres=${genreId}`)
       .then(apiData => {
         console.log(`*** apiData.results[0]:`, apiData.results[0])
 
@@ -70,4 +63,86 @@ result data:
     .name
   .vote_average
   .popularity
+
+  {
+  "genres": [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+}
+
 */
