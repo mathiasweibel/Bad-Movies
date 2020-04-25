ed91380cedc45b0cfa27ee16ef9b26bac7e10c42
Mathias <mathias2702@gmail.com>
2020-04-25 14:58:55 -0400

    Fix movie render bug

--- client/src/components/Movies.jsx
+++ client/src/components/Movies.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 class Movies extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      //
-    }
+    this.favorite = this.favorite.bind(this)
   }
 
   // Make an onClick for each list item. If the movies shown is the search results, 
@@ -13,6 +11,9 @@ class Movies extends React.Component {
 
   // If you're currently showing the fave list, delete the movie instead
   // You can tell which list is currently being rendered based on whether the prop "showFaves" is false (search results) or true (fave list) (within index.jsx)
+
+  // componentDidMount () {
+  // }
 
   favorite (movieObj) {
     console.log(`::: favorite() | movieObj.title:`, movieObj.title)
@@ -22,24 +23,25 @@ class Movies extends React.Component {
   render() {
     return (
       <ul className="movies">
-
         {this.props.movies.map(movie => {
-          <li className="movie_item" onClick={this.favorite(movie)}>
-            <img src={movie.img} />
-            <div className="movie_description">
-              <h2>{movie.title}</h2>
-              <section className="movie_details">
-                <div className="movie_year">
-                  <span className="title">Year</span>
-                  <span>{movie.year}</span>
-                </div>
-                <div className="movie_rating">
-                  <span className="title">Rating</span>
-                  <span>{movie.rating}</span>
-                </div>
-              </section>
-            </div>
-          </li>
+          return (
+            <li className="movie_item" onClick={this.favorite(movie)} key={movie.id}>
+              <img src={movie.img} />
+              <div className="movie_description">
+                <h2>{movie.title}</h2>
+                <section className="movie_details">
+                  <div className="movie_year">
+                    <span className="title">Year</span>
+                    <span>{movie.year}</span>
+                  </div>
+                  <div className="movie_rating">
+                    <span className="title">Rating</span>
+                    <span>{movie.rating}</span>
+                  </div>
+                </section>
+              </div>
+            </li>
+          )
         })}
 
 
@@ -66,3 +68,7 @@ class Movies extends React.Component {
 }
 
 export default Movies;
+
+// NOTES
+// You need a return inside the .map function IF you are returning something other than
+// a component.
